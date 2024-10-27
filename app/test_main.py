8f45d8d23e111a72f7d56e5b963193c095640ba1
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2024-10-27 12:52:46 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,31 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, expected_result",
+    [
+        pytest.param(
+            "playgrounds", True,
+            id="returns True if all letters in the word are different"
+        ),
+        pytest.param(
+            "look", False,
+            id="returns False if some letters are repeated"
+        ),
+        pytest.param(
+            "Adam", False,
+            id="returns False if the word has the same "
+               "uppercase and lowercase letters"
+        ),
+        pytest.param(
+            "", True,
+            id="returns True if an empty string is passed"
+        )
+    ]
+)
+def test_whether_the_word_is_an_isogram(
+        word: str,
+        expected_result: bool
+) -> None:
+    assert is_isogram(word) == expected_result
